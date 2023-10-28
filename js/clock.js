89bc537807c0a412dfa263358b5baae76d374eaa
Mateusz Oleksy <45857054+mateuszoleksy@users.noreply.github.com>
2023-10-29 01:35:55 +0200

    Version 0.0.1
    ALPHA

---
+++ js/clock.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1);
+function setClock()
+    {
+        let minute = document.getElementById("minute");
+        let hour = document.getElementById("hour");
+        let second = document.getElementById("second");
+
+        let date = new Date();
+        let milliseconds = date.getMilliseconds() / 1000;
+        let seconds = (date.getSeconds() + milliseconds) / 60;
+        let minutes = (seconds +  date.getMinutes()) / 60;
+        let hours = (minutes + date.getHours()) / 12;
+
+        setRotation(second, seconds);
+        setRotation(minute, minutes);
+        setRotation(hours, hours);
+    }
+
+    function setRotation(element, ratio) {
+        ratio = ratio * 360;
+        element.style.setProperty("--rotation", ratio);
+    }
